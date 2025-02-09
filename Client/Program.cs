a1a5a7da165af3f82bb16433e091199b6476db88
Brady Pacheco <bpacheco@students.ccu.edu>
2025-02-08 17:45:01 -0700

    Arrow keys and selection work

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,18 +1,64 @@
-﻿using Client.Library;
+﻿using System.Formats.Asn1;
+
+using Client.Library;
 
 Console.Clear();
+Console.CursorVisible = false;
+/* Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.White; */
+
 
 while (true)
 {
-    string[] items = await Api.GetStringArrayAsync();
-    int ItemNumber = 1;
     int ItemSelection = 1;
-            foreach (string item in items)
+    string[] items = await Api.GetStringArrayAsync();
+    while (true) {
+        int ItemNumber = 1;
+        Console.Clear();
+                foreach (string item in items)
+                {
+                    if (ItemNumber == ItemSelection)
+                    {
+                        Screen.Print1($"{ItemNumber}. {item}", foreground: ConsoleColor.Black, background: ConsoleColor.White);
+                    }
+                    else
+                    {
+                    Screen.Print1($"{ItemNumber}. {item}");
+
+                    }
+                    ItemNumber++;
+                }
+        
+        ConsoleKey KeyPress = Console.ReadKey().Key;
+        if (KeyPress == ConsoleKey.UpArrow)
+        {
+            if (ItemSelection < 1)
             {
-                Screen.Print1($"{ItemNumber}. {item}");
-                ItemNumber++;
+                ItemSelection = items.Length + 1;
             }
-    
-    Console.ReadKey();
+            else
+            {
+                ItemSelection--;
+            }
+        }
+        else if (KeyPress == ConsoleKey.DownArrow)
+        {
+            if (ItemSelection > items.Length)
+            {
+                ItemSelection = 1;
+            }
+            else
+            {
+                ItemSelection++;
+            }
+        }
+        else if (KeyPress == ConsoleKey.Enter)
+        {
+            Console.Clear();
+            Console.WriteLine($"You have selected {items[ItemSelection - 1]}");
+            Console.ReadKey();
+            break;
+        }
+    }
 }
 
